d5d0343d506cf1d07eafa8a230fbb50a710ba05d
Rohit Dhingra <Rohit Dhingra@DESKTOP-TQ58HNN>
2019-08-10 13:18:51 +0530

    Adding time also

--- JavaCodingProject/src/com/rohit/poc/ThreadSafetyExample.java
+++ JavaCodingProject/src/com/rohit/poc/ThreadSafetyExample.java
@@ -11,10 +11,13 @@ class Counter {
 public class ThreadSafetyExample {
 
 	public static void main(String[] args) {
+		long initial = System.currentTimeMillis();
 		Counter c = new Counter();
 		for (int i = 0; i < 2000; i++) {
 			c.increment();
 		}
+		long dur = System.currentTimeMillis() - initial;
+		System.out.println("Time take to process using single thread"+dur+"ms");
 		System.out.println(c.count);
 
 	}
