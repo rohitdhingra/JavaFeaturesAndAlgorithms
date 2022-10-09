0a63066d6902650ba413cf8d5d8d1d8b1f40dd1e
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2022-10-09 10:24:45 +0530

    toMap feature

--- Java8Features/src/com/leaning/poc/SkipAndLimitExample.java
+++ Java8Features/src/com/leaning/poc/SkipAndLimitExample.java
@@ -2,6 +2,8 @@ package com.leaning.poc;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SkipAndLimitExample {
 
@@ -9,6 +11,8 @@ public class SkipAndLimitExample {
 		List<Integer> intList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 		//2 to 8 values
 		intList.stream().skip(1).limit(7).forEach(System.out::println);
+		Map<Integer, Integer> collect = intList.stream().collect(Collectors.toMap(x->x,y -> y*100));
+		System.out.println(collect);
 	}
 
 }
